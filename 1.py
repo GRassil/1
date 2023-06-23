37ab8a35f2759aded6d91e9a12feebe130095246
Rassll <gabdelganievrasil@gmail.com>
2023-06-23 18:15:00 +0300

    В функции строка проходит через цикл. Если символы с индексами i и -i-1 совпадают, то итерации продолжаются. Если символы не совпадут, возвращается False и происходит выход из функции

--- 1.py
+++ 1.py
@@ -2,10 +2,9 @@ def palindrom_checking(some_string: str):
     some_string_len = len(some_string)//2 if len(some_string)%2==0 else len(some_string)//2+1
     for i in range(some_string_len):
         if some_string[i] != some_string[-i-1]:
-            return some_string_len
-    return some_string_len
+            return False
+    return True
 
 if __name__ == '__main__':
-    print(palindrom_checking('leppel'))
-    print(palindrom_checking('lepel'))
-    print(palindrom_checking('lepgnel'))
+    print(palindrom_checking('лепсспел'))
+    print(palindrom_checking('helloworld'))
