4bd5f4915325c2de8aea44f03c52b43182f1bc05
Rassll <gabdelganievrasil@gmail.com>
2023-07-11 18:52:21 +0300

    5 lesson

--- 1.py
+++
@@ -1,9 +0,0 @@
-def palindrom_checking(some_string: str):
-    for i in range(len(some_string)):
-        if some_string[i] != some_string[-i-1]:
-            return False
-    return True
-
-if __name__ == '__main__':
-    print(palindrom_checking('лепсспел'))
-    print(palindrom_checking('helloworld'))
